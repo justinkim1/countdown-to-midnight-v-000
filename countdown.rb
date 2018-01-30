9a7133362962b56af3347d9edb97ec9fd3aea248
Justin K <justin-kim1@hotmail.com>
2018-01-30 04:05:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ integer = 12
 
 def countdown(integer)
   while integer > 0 
-  puts `#{integer} SECOND(S)!`
+  puts "#{integer} SECOND(S)!"
   integer -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
